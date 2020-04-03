0f1c63791ad35b438f0e84d1a664bf88be3912e4
krzwier <krzwier@gmail.com>
2020-04-03 09:22:39 -0500

    Initiate Maven project with JUnit5.

---
+++ src/test/java/krzwier/AppTest.java
@@ -0,0 +1,19 @@
+package krzwier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test suit for String Calculator App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
